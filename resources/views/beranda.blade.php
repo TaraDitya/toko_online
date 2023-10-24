1ab5e8c9763776bf0a142965126fa302292f4736
TaraDitya <taradityaputra@gmail.com>
2023-10-24 10:32:00 +0700

    memperbarui popup jam oprasional

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,7 +1,26 @@
+@extends('toko.toko')
 
-<div class="container">
-    <div class="beranda-content">
-        <img src="./imgs/1662slide2.jpg" alt="slide1" width=500>
-        <img src="./imgs/3657slide3.jpg" alt="slide1" width=500>
+<style>
+    img {
+        border-radius: 15px;
+    }
+    
+    #angka {
+        opacity: 0;
+        visibility: hidden;
+        margin: 0;
+    }
+</style>
+@section('konten')
+    <div class="container"> 
+        @for($i = 0; $i <= 2; $i++)
+        <div class="beranda-content" style="margin: 0;">
+            <p id="angka" >{{ $i }}</p>
+            <img src="./imgs/1662slide2.jpg" class="border border-dark-subtle" alt="gambar1" width=1000>
+            <br><br>
+            <img src="./imgs/3657slide3.jpg" class="border border-dark-subtle" alt="gambar1" width=1000>
+        </div>
+        @endfor
+            
     </div>
-</div>
+@endsection
---
+++ resources/views/toko/kontrol.blade.php
@@ -0,0 +1,15 @@
+    <!-- list klik-->
+    <div class="list container mt-3">
+        <ul>
+            <a href="/beranda">
+                <li class="p-2">Beranda</li>
+            </a>
+            <a href="/produk">
+                <li class="p-2">Produk</li>
+            </a>
+            <a href="/ulasan">
+                <li class="p-2">Ulasan</li>
+            </a>
+        </ul>
+        <hr>
+    </div>
